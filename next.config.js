d287a29e9c487e75983493f11b9842498b58649a
jkantyukova <jkantyukova@gmail.com>
2023-09-14 15:57:10 +0300

    updateDomain

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@
 // module.exports = nextConfig
 module.exports = {
   images: {
-    domains: ['localhost'], // Добавьте здесь домены, с которыми работает ваше приложение
+    domains: ['https://todoback-0ish.onrender.com'], // Добавьте здесь домены, с которыми работает ваше приложение
   },
   env: {
     PORT_BACKEND: process.env.PORT_BACKEND || 'http://localhost:4000',
